3b11444a799e3ed72d4349ed01174f8ce995d416
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-03 18:04:53 +0300

    Add role to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'simple user' do
+    let(:user) { create(:user) }
+
+    context 'should' do
+      it 'have user role by default after create' do
+        expect(user.user?).to be true
+      end
+    end
+  end
 end
