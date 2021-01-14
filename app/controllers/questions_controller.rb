20a4b23117c352d64fc037bb1680adbca04012d9
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-14 20:28:27 +0300

    Fix problems for answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -74,6 +74,6 @@ class QuestionsController < ApplicationController
     end
 
     def question_params
-      params.require(:question).permit(:title, answers_attributes: [:title, :correct])
+      params.require(:question).permit(:title, answers_attributes: [:id, :title, :correct])
     end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   audited
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :answers
+  has_many :answers, inverse_of: :question
 
   validates :title, uniqueness: true,
                     presence:   true
@@ -10,7 +10,7 @@ class Question < ApplicationRecord
   validate :check_uniq_answers
   validates :answers, length: { is: 4, message: 'must be four' }
 
-  accepts_nested_attributes_for :answers
+  accepts_nested_attributes_for :answers, reject_if: :all_blank
 
   private
 
