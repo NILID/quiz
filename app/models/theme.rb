a915a67eb52e5745201a24687bd3eb80c023d607
DA <da@vpodolske.com>
2021-01-15 14:26:35 +0300

    Add counter cache to theme

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,5 +1,7 @@
 class Theme < ApplicationRecord
-  validates :title, uniqueness: true,
-                    presence:   true
-  belongs_to :question, optional: true
+  validates  :title,    uniqueness:    true,
+                        presence:      true
+
+  belongs_to :question, optional:      true,
+                        counter_cache: true
 end
