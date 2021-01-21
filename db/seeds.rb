7481e07a6e104cfde73cfa6e41c6c928a420a518
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-21 23:31:10 +0300

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
 author = User.first
 
-themes = [
-'Загадки',
-'Политики',
-]
+themes = [{
+         title:'Политики',
+     questions: [
+                  {title: 'Назовите президента России', answers_attributes: { '0': {title: "Путин", correct: true}, '1': {title: "Горбачев"}, '2': {title: "Сталин"}, '3': {title: "Навальный"}}},
+                  {title: 'Назовите президента США',    answers_attributes: { '0': {title: "Трамп", correct: true}, '1': {title: "Байден"},   '2': {title: "Кличко"}, '3': {title: "Навальный"}}}
+                ]
+}]
 
-themes.each { |theme| Theme.create(title: theme) }
+themes.each do |t|
+  theme = Theme.create!(title: t[:title])
 
-Question.create!(title: 'Назовите президента России', author: author, theme: Theme.last, answers_attributes: { '0': {title: "Путин", correct: true}, '1': {title: "Горбачев"}, '2': {title: "Сталин"}, '3': {title: "Навальный"} })
+  t[:questions].each do |question|
+    Question.create!(title: question[:title], author: author, theme: theme, answers_attributes: question[:answers_attributes])
+  end
+end
