9102040ab2de411d425da66989c8b55450541717
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-23 21:23:55 +0300

    Add next-button

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,8 @@
+// disable href for all links
+//
+
 var el = document.getElementById('timer'),
+    next_button = document.getElementById('next-button'),
     total = 15,
     url = el.dataset.url
     timeinterval = setInterval(function () {
@@ -6,6 +10,11 @@ var el = document.getElementById('timer'),
         el.textContent = total;
         if (total <= 0) {
             clearInterval(timeinterval);
-            window.location = url;
-        }
+            var buttons = document.getElementsByClassName("button");
+            for (var i = 0, len = buttons.length; i < len; i++) {
+              var button = buttons[i];
+              button.href="javascript:;";
+              next_button.classList.remove('d-none');
+            }
+         }
     }, 1000);
