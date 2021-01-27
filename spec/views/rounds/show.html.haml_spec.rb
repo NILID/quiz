fa8c929c6e876c98c346581293dd00261ef18935
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-27 20:43:53 +0300

    Remove unneeded page

--- spec/views/rounds/show.html.haml_spec.rb
+++ spec/views/rounds/show.html.haml_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "rounds/show", type: :view do
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/<a class="btn" data-remote="true" href="\/rounds\/#{@round.id}\?page=1">Продолжить<\/a>/)
+    expect(rendered).to match(/<a class="btn" data-remote="true" href="\/rounds\/#{@round.id}">Продолжить<\/a>/)
   end
 end
 
