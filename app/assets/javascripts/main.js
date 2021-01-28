cea97026b8e1518bf22339c9a2f67e8b8a5033a9
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-28 20:28:10 +0300

    Rebuild js scripts

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,26 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var newThemeButton = document.getElementById('add-theme');
+function getThemeForm(){
   var closeModal     = document.getElementById('modal_close');
   var themeModal     = document.getElementById('theme_modal');
   var themeErrors    = document.getElementById('theme_errors');
   var themeForm      = document.getElementById('new_theme_form');
 
-  if (typeof(newThemeButton) != 'undefined' && newThemeButton != null)
-  {
-    newThemeButton.addEventListener('click', function() {
-      themeModal.classList.remove('d-none');
-    });
-  };
+  themeModal.classList.remove('d-none');
 
-  if (typeof(closeModal) != 'undefined' && closeModal != null)
-  {
-    closeModal.addEventListener('click', function() {
-      // Remove old errors
-      // Reset and hide form
+  closeModal.addEventListener('click', function() {
 
-      themeModal.classList.add('d-none');
-      themeErrors.innerHTML = "";
-      themeForm.reset();
-    });
-  };
-});
+    // Remove old errors
+    // Reset and hide form
+    themeModal.classList.add('d-none');
+    themeErrors.innerHTML = "";
+    themeForm.reset();
+  });
+};
+
+function selectOnlyOneCheckbox(id){
+  var myCheckbox = document.getElementsByClassName("answerCheckbox");
+  Array.prototype.forEach.call(myCheckbox,function(el){
+    el.checked = false;
+  });
+  id.checked = true;
+}
