28984dc7c438b172867641eb865ece9534153fc4
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-28 21:10:20 +0300

    Add translations

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -6,7 +6,7 @@ class Audit < Audited::Audit
       when 'Question' then auditable&.title
       when 'User'     then auditable&.email
       end
-    [I18n.t("activerecord.models.#{auditable_type.downcase}"), 'with id =', auditable_id, name].join(' ')
+    [I18n.t("activerecord.models.#{auditable_type.downcase}"), 'ID #', auditable_id, name].join(' ')
   end
 
 end
