a8c09da8369aafd55af7ac3cb60bc4454fa19ef5
da <dm.ilin.aleksandrovich@gmail.com>
2021-01-29 19:35:08 +0300

    Add initializer for switch user

---
+++ config/initializers/switch_user.rb
@@ -0,0 +1,9 @@
+SwitchUser.setup do |config|
+  config.provider = :devise
+  config.available_users = { user: -> { User.all } }
+  config.available_users_identifiers = { user: :id }
+  config.available_users_names = { user: :email }
+  config.controller_guard = ->(current_user, request) { true }
+  config.view_guard = ->(current_user, request) { true }
+  config.helper_with_guest = true
+end
